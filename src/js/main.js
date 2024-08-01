ba7133ae479b4737eb8cce908954d95ec4424937
Tsiporenko <ciporenkovaceslav@gmail.com>
2024-08-01 15:15:46 +0300

    move progect on vite

---
+++ src/js/main.js
@@ -0,0 +1,9 @@
+import '../scss/style.scss'
+
+const iconMenu = document.querySelector('.icon-menu');
+const headerMenu = document.querySelector('.header__menu');
+
+iconMenu.addEventListener('click', () => {
+  headerMenu.classList.toggle('menu-open');
+  iconMenu.classList.toggle('menu-open');
+});
